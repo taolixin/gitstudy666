a105b39f3f4b4a0779df848037ecb6f9a9b8803b
taolixin <1569253040@qq.com>
2023-01-07 11:01:31 +0800

    修改

--- my-project666/src/views/AboutView.vue
+++ my-project666/src/views/AboutView.vue
@@ -8,7 +8,7 @@
           <span class="el-dropdown-link">
             <el-avatar src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png" />
             <span class="el-dropdown-link">
-              欢迎：18742518888
+              欢迎：18742518888111
               <el-icon class="el-icon--right">
                 <arrow-down />
               </el-icon>
